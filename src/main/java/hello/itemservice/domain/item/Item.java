2c887406222db9e6f5e35ab7dab12483d67315a4
sarana-hub <81903928+sarana-hub@users.noreply.github.com>
2022-02-07 18:25:39 +0900

    Update Item.java

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -3,17 +3,21 @@ package hello.itemservice.domain.item;
 import lombok.Getter;
 import lombok.Setter;
 
-//@Data
+
+/*상품 객체*/
+
 @Getter @Setter
 public class Item {
     private Long id;
     private String itemName;
-    private Integer price;
+    private Integer price;      //null일 경우 포함하기 위해 Integer
     private Integer quantity;
 
+    //기본 생성자
     public Item() {
     }
 
+    //id 제외한 생성자
     public Item(String itemName, Integer price, Integer quantity) {
         this.itemName = itemName;
         this.price = price;

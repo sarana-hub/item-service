da50230ac3fb11f21d583b1fc165f099b630186f
sarana-hub <81903928+sarana-hub@users.noreply.github.com>
2022-02-11 16:35:08 +0900

    Update ItemRepository.java

--- src/main/java/hello/itemservice/domain/item/ItemRepository.java
+++ src/main/java/hello/itemservice/domain/item/ItemRepository.java
@@ -15,23 +15,23 @@ public class ItemRepository {
     private static final Map<Long, Item> store=new HashMap<>();  
     private static  long sequence=0L;   
 
-    public Item save(Item item){
+    public Item save(Item item){    //상품 저장 기능
         item.setId(++sequence);
         store.put(item.getId(), item);
         return item;
     }
 
-    public Item findById(Long id){
+    public Item findById(Long id){      //조회 
         return store.get(id);
     }
 
-    public List<Item> findAll(){
+    public List<Item> findAll(){        //전체 조회
         return new ArrayList<>(store.values());
     }
 
-    public void update(Long itemId, Item updateParam){
+    public void update(Long itemId, Item updateParam){      //업데이트      //파라미터 정보(이름,가격,수량)
         Item findItem=findById(itemId);
-        findItem.setItemName(updateParam.getItemName());
+        findItem.setItemName(updateParam.getItemName());    
         findItem.setPrice(updateParam.getPrice());
         findItem.setQuantity(updateParam.getQuantity());
     }

9fa1270f95ab8a217b74610720c7bd9e72ccc9a5
nayeon-jo <nyc0516@naver.com>
2022-12-28 19:57:09 +0900

    파일 업로드 기능

---
+++ src/main/java/hello/itemservice/web/basic/ItemForm.java
@@ -0,0 +1,18 @@
+package hello.itemservice.web.basic;
+
+import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+/**상품 저장용 폼*/
+
+@Data
+public class ItemForm {
+    private Long id;
+    private String itemName;
+    private Integer price;
+    private Integer quantity;
+
+    private List<MultipartFile> imageFiles;
+}
